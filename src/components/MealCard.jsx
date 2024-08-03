ad75380cb948084e29f7826797612cc78a2b79b6
ankitgupta-13 <guptankit0522@gmail.com>
2024-08-03 23:23:00 +0530

    tailwind configure

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const MealCard = ({ name, image }) => {
   return (
     <div className="flex">
       <p className="text-3xl font-bold underline">{name}</p>
-      <img src={image} alt="" />
+      <img src={image} alt="" className="w-4" />
     </div>
   );
 };
